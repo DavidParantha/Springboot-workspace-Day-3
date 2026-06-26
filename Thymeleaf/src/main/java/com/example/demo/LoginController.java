b6eb3353c37458c9cab163ae52526992ec9edf0f
David <paranthadavid26@gmail.com>
2026-06-26 11:06:52 +0530

    Day 3: Add Thymeleaf project

---
+++ Thymeleaf/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(@RequestParam String username,
+                              @RequestParam String password,
+                              Model model) {
+
+        if (username.equals("admin") && password.equals("1234")) {
+            model.addAttribute("username", username);
+            return "welcome";
+        } else {
+            model.addAttribute("error", "Invalid Username or Password");
+            return "login";
+        }
+    }
+}
